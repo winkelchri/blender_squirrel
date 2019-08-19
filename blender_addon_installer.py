67966f34f83d54cae8a85e054c4072b37de219df
Christopher Winkelmann <winkelchri@gmail.com>
2019-08-19 23:54:25 +0200

    Updated install command of cli.

--- blender_addon_installer.py
+++ blender_addon_installer.py
@@ -1,14 +1,34 @@
 import click
 
+from loguru import logger
+
 from utils.settings import AddonInstallerSettings
-from utils.installers import ZipPluginInstaller
-from utils.plugin_finders import ZipPluginFinder
+from utils.plugins import ZipPlugin
+from utils.plugins.validators import InvalidBlenderPlugin
+# from utils.installers import ZipPluginInstaller
+# from utils.plugin_finders import ZipPluginFinder
 
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
 def install():
     settings = AddonInstallerSettings()
-    downloaded_addons = settings.find_downloaded_plugins()
+    plugins_paths = settings.find_downloaded_plugins()
+
+    for plugin_path in plugins_paths:
+        plugin = ZipPlugin(
+            settings=settings,
+            plugin_filename=plugin_path
+        )
+        try:
+            plugin.install()
+        except InvalidBlenderPlugin as e:
+            click.echo(f"Skipping {plugin_path} - invalid blender plugin")
+
 
 if __name__ == "__main__":
-    pass
+    cli()
