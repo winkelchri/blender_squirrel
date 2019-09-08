cf05a627c2e2e2d5a2e5e3b1e034c2125b61afd8
Christopher Winkelmann <winkelchri@gmail.com>
2019-09-08 17:47:48 +0200

    WIP: find_downloaded_addons has to be rewritten

--- tests/test_settings_os_aware.py
+++ tests/test_settings_os_aware.py
@@ -4,6 +4,8 @@ from loguru import logger
 from pathlib import Path
 
 from utils.settings import AddonInstallerSettings
+from fixtures import valid_addons
+from fixtures import zip_file
 
 logger.add('log/test_settings_os_aware.log', rotation="1 MB")
 
@@ -27,4 +29,8 @@ def found_download_test_addons(settings, downloaded_addons_path):
 
 
 def test_find_downloaded_addons(found_download_test_addons):
+    # for file in valid_addons:
+    #     zip_file = zip_file(file)
+
+
     assert len(found_download_test_addons) == 4
