622c77515aa8b26624451a19c778b65954cf919c
Christopher Winkelmann <winkelchri@gmail.com>
2020-03-24 00:50:50 +0100

    WIP: Created manager pattern for controlling addons.

---
+++ squirrel/addons/manager.py
@@ -0,0 +1,31 @@
+
+
+class BlenderAddonManager():
+    ''' Class to manage all workflows around addon
+        installation and update.
+    '''
+
+    def __init__(self):
+        pass
+
+    def index(self):
+        ''' Indexes all addons and save its metadata
+            into the addon database.
+        '''
+
+        pass
+
+    def poll_remote_sources(self):
+        ''' Polls remote addon sources to gather
+            recent addon versions.
+        '''
+        pass
+
+    def download_addon(self):
+        pass
+
+    def install_addon(self):
+        pass
+
+    def remove_addon(self):
+        pass
